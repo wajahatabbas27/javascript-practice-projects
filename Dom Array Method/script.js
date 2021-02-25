5f568db1748bd0a47151a74e5cc60235b8551cc5
wajahataz <54248569+wajahatabbas27@users.noreply.github.com>
2021-02-25 10:52:50 +0500

    html-css done for dom array methods

---
+++ Dom Array Method/script.js
@@ -0,0 +1,8 @@
+//callinng Dom elements
+const main = document.getElementById('main');
+const addUserButton = document.getElementById('add-user');
+const doubleMoney = document.getElementById('double-money');
+const showMillionaire = document.getElementById('show-millionaire');
+const sortButton = document.getElementById('sort-richest');
+const totalButton = document.getElementById('calculate-total');
+
